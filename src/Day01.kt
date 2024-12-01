8c5008d12a7a80eafd69fa6b151b4aca6b00d3a1
Máté Molnár <matemolnar@protonmail.com>
2024-12-01 12:44:52 +0100

    day01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,10 +1,31 @@
+import kotlin.math.abs
+
 fun main() {
+    fun parseLocations(input: List<String>): Pair<MutableList<Int>, MutableList<Int>> {
+        val locationLeft = mutableListOf<Int>()
+        val locationRight = mutableListOf<Int>()
+        for (line in input) {
+            val lineValues = line.split("   ")
+            locationLeft.add(lineValues[0].toInt())
+            locationRight.add(lineValues[1].toInt())
+        }
+        return Pair(locationLeft, locationRight)
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return parseLocations(input).let { (left, right) ->
+            left.sort()
+            right.sort()
+            Pair(left, right)
+        }.let { (sortedLeft, sortedRight) ->
+            sortedLeft.zip(sortedRight).sumOf { (left, right) -> abs(left - right) }
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        parseLocations(input).let { (locationLeft, locationRight) ->
+            return locationLeft.sumOf { left -> left * locationRight.count { it == left } }
+        }
     }
 
     val input = readInput("Day01")
