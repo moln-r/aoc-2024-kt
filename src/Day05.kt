2cc18bb1ff5e4f200c8f953e83c13582e324aba3
Máté Molnár <matemolnar@protonmail.com>
2024-12-05 15:00:28 +0100

    day05.2 solution

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,22 +1,21 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        val rules: Map<String, MutableList<String>> = input.filter { it.contains('|') }
-            .map { it.split('|') }
-            .groupByTo(mutableMapOf(), { it[0].trim() }, { it[1].trim() })
+        val (rules, updateOrders) = parse(input)
 
-        val updateOrders = input.filter { it.contains(',') }
-            .map { it.split(',') }
-            .map { UpdateOrder.from(it) }
-            .toList()
+        val solver = Solver(rules)
 
-        val validator = Validator(rules)
-
-        return updateOrders.filter { validator.run(it) }
+        return updateOrders.filter { solver.simpleValidation(it) }
             .sumOf { it.middle }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (rules, updateOrders) = parse(input)
+
+        val solver = Solver(rules)
+
+        return updateOrders.filter { !solver.simpleValidation(it) }
+            .map { solver.fix(it) }
+            .sumOf { it.middle }
     }
 
     val input = readInput("Day05")
@@ -24,8 +23,21 @@ fun main() {
 
     check(143 == part1(testInput))
     part1(input).println()
-
+    check(123 == part2(testInput))
     part2(input).println()
+}
+
+fun parse(input: List<String>): Pair<MutableMap<String, MutableList<String>>, List<UpdateOrder>> {
+    val rules = input.filter { it.contains('|') }
+        .map { it.split('|') }
+        .groupByTo(mutableMapOf(), { it[0].trim() }, { it[1].trim() })
+
+    val updateOrders = input.filter { it.contains(',') }
+        .map { it.split(',') }
+        .map { UpdateOrder.from(it) }
+        .toList()
+
+    return Pair(rules, updateOrders)
 }
 
 data class UpdateOrder(val order: List<String>, val middle: Int) {
@@ -38,28 +50,55 @@ data class UpdateOrder(val order: List<String>, val middle: Int) {
     }
 }
 
-class Validator(private val rules: Map<String, MutableList<String>>) {
-    fun run(updateOrder: UpdateOrder): Boolean {
+enum class Move {
+    MOVE_TO_END,
+    SWITCH,
+}
 
+class Solver(private val rules: Map<String, List<String>>) {
+
+    fun simpleValidation(updateOrder: UpdateOrder): Boolean {
+        return validate(updateOrder).first == null
+    }
+
+    fun validate(updateOrder: UpdateOrder): Triple<Move?, Int, Int> {
         for ((i, update) in updateOrder.order.withIndex()) {
-            val beforeCurrent = updateOrder.order.subList(0, i)
-            if (beforeCurrent.isNotEmpty()) {
-                val priorUpdates = beforeCurrent.map { rules[it] }.toList()
-                if (!priorUpdates.all { it!!.contains(update) }) {
-//                    println("$update is not supported by ${beforeCurrent.joinToString(",")}")
-                    return false
+            val startIndex = i + 1
+            for ((j, next) in updateOrder.order.subList(startIndex, updateOrder.order.size).withIndex()) {
+                if (rules[update] == null) {
+                    return Triple(Move.MOVE_TO_END, i, -1)
                 }
-            }
-
-            val afterCurrent = updateOrder.order.subList(i + 1, updateOrder.order.size)
-            if (afterCurrent.isNotEmpty()) {
-                if (!rules[update]!!.containsAll(afterCurrent)) {
-//                    println("$update is not supporting ${afterCurrent.joinToString(",")}")
-                    return false
+                if (rules[update]?.contains(next) == false) {
+                    return Triple(Move.SWITCH, i, startIndex + j)
                 }
             }
         }
-
-        return true
+        return Triple(null, -1, -1)
     }
+
+    fun fix(updateOrdersToFix: UpdateOrder): UpdateOrder {
+        val (move, i, j) = validate(updateOrdersToFix)
+        if (move == null) {
+            return updateOrdersToFix
+        }
+
+        val newOrder = when (move) {
+            Move.MOVE_TO_END -> {
+                val new = updateOrdersToFix.order.toMutableList()
+                new.add(new.removeAt(i))
+                new
+            }
+
+            Move.SWITCH -> {
+                val new = updateOrdersToFix.order.toMutableList()
+                val oldJ = new.removeAt(j)
+                val oldI = new.removeAt(i)
+                new.add(i, oldJ)
+                new.add(j, oldI)
+                new
+            }
+        }
+        return fix(UpdateOrder.from(newOrder))
+    }
+
 }
